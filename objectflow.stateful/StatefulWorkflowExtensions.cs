a24fabdec98c3228d16c3f06c76d02c0561520e3
Tim Kellogg <tkellogg@kpaonline.com>
2011-03-07 12:18:35 -0700

    Stateful functionality works All tests pass but I think I need to write more tests to verify structure and such

---
+++ objectflow.stateful/StatefulWorkflowExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rainbow.ObjectFlow.Stateful
+{
+    /// <summary>
+    /// Extension methods used to work with stateful workflows
+    /// and stateful objects
+    /// </summary>
+    public static class StatefulWorkflowExtensions
+    {
+        /// <summary>
+        /// Indicates that this object is currently passing through the specific workflow
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="workflowId"></param>
+        /// <returns></returns>
+        public static bool IsAliveInWorkflow(this IStatefulObject self, object workflowId)
+        {
+            return self.GetStateId(workflowId) != null;
+        }
+
+        /// <summary>
+        /// Indicates that this object is currently passing through the specific workflow
+        /// </summary>
+        /// <param name="self"></param>
+        /// <param name="workflow"></param>
+        /// <returns></returns>
+        public static bool IsAliveInWorkflow<T>(this IStatefulObject self, IStatefulWorkflow<T> workflow)
+            where T : class, IStatefulObject
+        {
+            return self.GetStateId(workflow.WorkflowId) != null;
+        }
+    }
+}
